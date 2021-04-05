a0e96fc369711e2fbc5ed42edcf4c5379aa60e09
Khouloudlaarous <khouloudaissa@gmail.com>
2021-04-06 00:09:11 +0100

    creat props and events

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,16 +2,18 @@
   <div>
      <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
     <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-    <div v-for="(todo, index) in todosFiltred" :key="todo.id" class="todo-item">
-      <div class="todo-item-left">
+    <todo-item v-for="(todo, index) in todosFiltred" :key="todo.id" :todo="todo" :index="index" @removedTodo="removeTodo">
+
+<!--      <div class="todo-item-left">
         <input type="checkbox" v-model="todo.completed">
         <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{todo.title}}</div>
         <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
       </div>
       <div class="remove-item" @click="removeTodo(index)">
         &times;
-      </div>
-    </div>
+      </div>-->
+
+    </todo-item>
     </transition-group>
     <div class="extra-container">
       <div><label><input type="checkbox" :checked="!anyRemainning" @change="checkAllTodos">Check All</label></div>
@@ -36,8 +38,13 @@
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
+
 export default {
   name: 'ToDo-list',
+  components: {
+    TodoItem,
+  },
   data () {
     return {
       newTodo: '',
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item-left">
+      <input type="checkbox" v-model="completed">
+      <div v-if="!editing" @dblclick="editTodo" class="todo-item-label" :class="{ completed : completed }">{{todo.title}}</div>
+      <input v-else class="todo-item-edit" type="text" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
+    </div>
+    <div class="remove-item" @click="removeTodo(index)">
+      &times;
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+    index:{
+      type: Number,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      'id': this.todo.id,
+      'title': this.todo.title,
+      'completed': this.todo.completed,
+      'editing': this.todo.editing,
+      'beforeEditCache': '',
+
+    }
+  },
+  methods: {
+    removeTodo(index) {
+      this.$emit('removedTodo', index)
+    }
+  }
+}
+</script>
