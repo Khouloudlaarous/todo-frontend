c85009dbda54f2e2714ada3ddae1aa40fbfc5786
Khouloudlaarous <khouloudaissa@gmail.com>
2021-04-09 22:30:07 +0100

    creating a store folder

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -75,36 +75,29 @@ export default {
     eventBus.$on('removedTodo', (index) => this.removeTodo(index))
     eventBus.$on('finishedEdit', (data) => this.finishedEdit(data))
     eventBus.$on('checkAllChanged', (checked) => this.checkAllTodos(checked))
-    eventBus.$on('filterChanged', (filter) => this.filter = filter)
+    eventBus.$on('filterChanged', (filter) => this.$store.state.filter = filter)
     eventBus.$on('clearCompletedTodos', () => this.clearCompleted ())
   },
   beforeDestroy() {
     eventBus.$off('removedTodo', (index) => this.removeTodo(index))
     eventBus.$off('finishedEdit', (data) => this.finishedEdit(data))
     eventBus.$off('checkAllChanged', (checked) => this.checkAllTodos(checked))
-    eventBus.$off('filterChanged', (filter) => this.filter = filter)
+    eventBus.$off('filterChanged', (filter) => this.$store.state.filter = filter)
     eventBus.$off('clearCompletedTodos', () => this.clearCompleted ())
   },
   computed: {
     remaining() {
-      return this.todos.filter(todo => !todo.completed).length
+      return this.$store.getters.remaining
     },
 
     anyRemaining() {
-      return this.remaining !== 0
+      return this.$store.getters.anyRemaining
   },
     todosFiltered() {
-      if (this.filter == 'all') {
-        return this.todos
-      } else if (this.filter == 'active') {
-        return this.todos.filter(todo => !todo.completed)
-      } else if (this.filter == 'completed') {
-        return this.todos.filter(todo => todo.completed)
-      }
-       return this.todos
+      return this.$store.getters.todosFiltered
     },
     showClearCompletedButton() {
-      return this.todos.filter(todo => todo.completed).length > 0
+      return this.$store.getters.showClearCompletedButton
     }
   },
 
@@ -112,33 +105,34 @@ export default {
   methods: {
     addTodo() {
       if (this.newTodo.trim().length == 0) {
-        return
+        return;
       }
 
-      this.todos.push({
+      this.$store.state.todos.push({
         id: this.idForTodo,
         title: this.newTodo,
         completed: false,
-      })
+      });
 
       this.newTodo = ''
       this.idForTodo++
      },
 
      removeTodo(index) {
-      this.todos.splice(index, 1)
+       this.$store.state.todos.splice(index, 1)
      },
 
     checkAllTodos() {
-      this.todos.forEach((todo) => todo.completed = event.target.checked)
+      this.$store.state.todos.forEach((todo) => todo.completed = event.target.checked)
     },
 
     clearCompleted() {
-      this.todos = this.todos.filter(todo => !todo.completed)
+      this.$store.state.todos = this.$store.state.todos.filter(todo => !todo.completed)
     },
 
     finishedEdit(data) {
-      this.todos.splice(data.index, 1, data.todo)
+      const index = this.$store.state.todos.findIndex(item => item.id == data.id);
+      this.$store.state.todos.splice(data.index, 1, data)
     }
     }
 }
---
+++ src/store/store.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    filter: 'all',
+    todos: [
+      {
+        'id': 1,
+        'title': 'Finish Vue Screencast',
+        'completed': false,
+        'editing' : false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over Word',
+        'completed': false,
+        'editing' : false,
+      },
+    ]
+  },
+  getters: {
+    remaining(state) {
+      return state.todos.filter(todo => !todo.completed).length
+    },
+
+    anyRemaining(state, getters) {
+      return getters.remaining !== 0
+    },
+    todosFiltered(state) {
+      if (state.filter == 'all') {
+        return state.todos
+      } else if (state.filter == 'active') {
+        return state.todos.filter(todo => !todo.completed)
+      } else if (state.filter == 'completed') {
+        return state.todos.filter(todo => todo.completed)
+      }
+      return state.todos
+    },
+    showClearCompletedButton(state) {
+      return state.todos.filter(todo => todo.completed).length > 0
+    }
+  }
+})
