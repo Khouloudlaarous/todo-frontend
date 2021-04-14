1179c7f1aec22c6d636d19af8047b9f6c7292217
Khouloudlaarous <khouloudaissa@gmail.com>
2021-04-14 22:50:33 +0100

    finish store editing

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -45,36 +45,14 @@ export default {
       newTodo: '',
       idForTodo: 3,
       beforeEditCache: '',
-      filter: 'all',
-      todos: [
-        {
-          'id': 1,
-          'title': 'Finish Vue Screencast',
-          'completed': false,
-          'editing' : false,
-        },
-        {
-          'id': 2,
-          'title': 'Take over Word',
-          'completed': false,
-          'editing' : false,
-        },
-      ]
     }
   },
   computed: {
-    remaining() {
-      return this.$store.getters.remaining
-    },
-
     anyRemaining() {
       return this.$store.getters.anyRemaining
   },
     todosFiltered() {
       return this.$store.getters.todosFiltered
-    },
-    showClearCompletedButton() {
-      return this.$store.getters.showClearCompletedButton
     }
   },
 
--- src/store/store.js
+++ src/store/store.js
@@ -78,22 +78,34 @@ export const store = new Vuex.Store({
 
   actions: {
     addTodo(context, todo) {
-     context.commit('addTodo', todo)
+      setTimeout(() => {
+        context.commit('addTodo', todo)
+      }, 1000)
     },
     updateTodo(context,todo) {
+      setTimeout(() => {
       context.commit('updateTodo', todo)
+      }, 1000)
     },
     deleteTodo(context, id) {
+      setTimeout(() => {
       context.commit('deleteTodo', id)
+    }, 1000)
     },
     checkAll(context, checked) {
+      setTimeout(() => {
       context.commit('checkAll', checked)
+}, 1000)
     },
     updateFilter(context, filter) {
+      setTimeout(() => {
       context.commit('updateFilter', filter)
+}, 1000)
     },
     clearCompleted(context) {
+      setTimeout(() => {
       context.commit('clearCompleted')
+}, 1000)
     }
   }
 
